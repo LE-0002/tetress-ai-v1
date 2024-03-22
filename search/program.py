772ea42580d65c787572d8bb9ba3935205d447c3
David Le <dlle2@student.unimelb.edu.au>
2024-03-22 23:18:46 +1100

    fixed bug in isEmpty function

--- search/program.py
+++ search/program.py
@@ -5,12 +5,15 @@ from .core import PlayerColor, Coord, PlaceAction
 from .utils import render_board
 import heapq
 
+
+tetromino = [(0,0), (1, 0), (2, 0), (3, 0)]
 # Basic structure of a node to be added to priority queue
 # Still to be modified as going along
 class Node: 
     def __init__(self, board: dict[Coord, PlayerColor], prevActions: list[PlaceAction]):
         self.board = board
         self.prevActions = prevActions
+
 
 
 def search(
@@ -41,10 +44,13 @@ def search(
     initialNode = Node(board, 4)
     heapq.heappush(priorityQueue, (0, initialNode)) 
 
+    print(isEmpty(board, Coord(1, 0)))
+
     # The render_board() function is handy for debugging. It will print out a
     # board state in a human-readable format. If your terminal supports ANSI
     # codes, set the `ansi` flag to True to print a colour-coded version!
     print(render_board(board, target, ansi=True))
+
 
     # Do some impressive AI stuff here to find the solution...
     # ...
@@ -60,3 +66,32 @@ def search(
         PlaceAction(Coord(1, 8), Coord(2, 8), Coord(3, 8), Coord(4, 8)),
         PlaceAction(Coord(5, 8), Coord(6, 8), Coord(7, 8), Coord(8, 8)),
     ]
+
+# Needs to be fixed, currently only doing it for one type of tetromino
+# Checks if tetromino can be placed on a particular square in board
+def validMove(board, square: Coord):
+    # If square is occupied, return
+    if not isEmpty(board, square):
+        return False 
+    # Check if tetromino can be placed      
+    for i in range(4):
+        for j in range(4):
+            if i==j: 
+                continue
+            elif isEmpty(board, square.__sub__tetromino[i]):
+                return True
+    return True
+                
+
+
+
+# Checks if square on board is unoccupied
+def isEmpty(board: dict[Coord, PlayerColor], square: Coord):
+    # If occupied
+    if board.get(square, None):
+        return False
+    else: 
+        return True
+
+
+
