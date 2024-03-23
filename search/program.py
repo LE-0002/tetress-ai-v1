b9b071132f21e356db8e36937d4d3f002a6c2c64
David Le <dlle2@student.unimelb.edu.au>
2024-03-23 12:53:45 +1100

    generates all possible tetromino placements given a square and adds it to the priority queue

--- search/program.py
+++ search/program.py
@@ -6,7 +6,31 @@ from .utils import render_board
 import heapq
 
 
-tetromino = [Coord(0,0), Coord(0, 1), Coord(0, 2), Coord(0, 3)]
+
+# Hardcoded the tetrominoes - not sure if this is best practice or whether
+# I should use rotation instead
+tetrominoes = [
+    [Coord(0,0), Coord(0,1), Coord(0, 2), Coord(0, 3)], # I
+    [Coord(0,0), Coord(1, 0), Coord(2, 0), Coord(3, 0)], # I
+    [Coord(0, 0), Coord(0, 1), Coord(1, 0), Coord(1, 1)], # O
+    [Coord(0, 0), Coord(0, 1), Coord(0, 2), Coord(1, 1)], # T
+    [Coord(0, 0), Coord(1, 0), Coord(2, 0), Coord(-1%11, 1)], # T
+    [Coord(1, 0), Coord(1, 1), Coord(1, 2), Coord(0, 1)], # T
+    [Coord(0, 0), Coord(1, 0), Coord(2, 0), Coord(1, 1)], # T
+    [Coord(0, 0), Coord(1, 0), Coord(2, 0), Coord(-1%11, 2)], # J
+    [Coord(0, 0), Coord(1, 0), Coord(1, 1), Coord(1, 2)], # J
+    [Coord(0, 0), Coord(0, 1), Coord(1, 0), Coord(2, 0)], # J
+    [Coord(0, 0), Coord(0, 1), Coord(0, 2), Coord(1, 2)], # J
+    [Coord(0, 0), Coord(1, 0), Coord(2, 0), Coord(2, 1)], # L
+    [Coord(0, 0), Coord(0, 1), Coord(0, 2), Coord(1, 0)], # L
+    [Coord(0, 0), Coord(0, 1), Coord(1, 1), Coord(2, 1)], # L
+    [Coord(1, 0), Coord(0, 2), Coord(1, 1), Coord(1, 2)], # L
+    [Coord(0, 0), Coord(0, 1), Coord(1, 1), Coord(1, 2)], # Z
+    [Coord(0, 1), Coord(1, 0), Coord(1, 1), Coord(2, 0)], # Z
+    [Coord(0, 1), Coord(0, 2), Coord(1, 0), Coord(1, 1)], # S
+    [Coord(0, 0), Coord(1, 0), Coord(1, 1), Coord(2, 1)] # S
+]
+
 # Basic structure of a node to be added to priority queue
 # Still to be modified as going along
 class Node: 
@@ -14,6 +38,8 @@ class Node:
         self.board = board
         self.prevActions = prevActions
 
+    def __lt__(self, other):
+        return len(self.prevActions) < len(other.prevActions)
 
 
 def search(
@@ -41,10 +67,19 @@ def search(
     # To remove, heapq.heappop(priorityQueue)[1]
 
     # Create starting node
-    initialNode = Node(board, 4)
+    initialNode = Node(board, [])
     heapq.heappush(priorityQueue, (0, initialNode)) 
 
-    print(validMove(board, Coord(1, 3)))
+    # This is just for debugging
+    print(validMove(board, Coord(7, 0), tetrominoes[0]))
+    for tetromino in tetrominoes:
+        if validMove(board, Coord(7, 0), tetromino):
+            for item in validMove(board, Coord(7, 0), tetromino):
+                newBoard = updateBoard(board, item)
+                heapq.heappush(priorityQueue, (0, Node(newBoard, [])))
+                print(render_board(newBoard, target, ansi=True))
+    print(len(priorityQueue))
+   
 
     # The render_board() function is handy for debugging. It will print out a
     # board state in a human-readable format. If your terminal supports ANSI
@@ -68,23 +103,34 @@ def search(
     ]
 
 
-# Checks if tetromino can be placed on a particular square in board (Currently only for one tetromino)
-def validMove(board, square: Coord):
+
+
+def updateBoard(board, actions: PlaceAction):
+    newBoard = board.copy()
+    newBoard[actions.c1] = newBoard[actions.c2] = PlayerColor.RED
+    newBoard[actions.c3] = newBoard[actions.c4] = PlayerColor.RED
+    return newBoard 
+
+# Returns all possible positions that tetromino can be placed on square. Returns a 2D array. 
+# Can change it to contain PlaceActions later. Keeping it as a 2D array so it's easier to print and debug.
+def validMove(board, square: Coord, tetromino: [Coord]):
+    total = []
     # If square is occupied, return
     if not isEmpty(board, square):
-        return False 
+        return None
     # Check if tetromino can be placed      
     for i in range(4):
         valid = True
+        actions = []
         for j in range(4):
-            if i==j: 
-                continue
             # Checks if block is filled. If filled, sets it to false
-            elif not isEmpty(board, square - tetromino[i] + tetromino[j]):
+            if not isEmpty(board, square - tetromino[i] + tetromino[j]):
                 valid = False
+            else: 
+                actions.append(square - tetromino[i] + tetromino[j])
         if valid:
-            return True        
-    return False
+            total.append(PlaceAction(actions[0], actions[1], actions[2], actions[3]))        
+    return total
                 
 
 
@@ -97,5 +143,11 @@ def isEmpty(board: dict[Coord, PlayerColor], square: Coord):
     else: 
         return True
 
+
+
+# For debugging - prints out a list of placeActions, easier to read
+def printPlaceAction(items: [PlaceAction]):
+    for item in items: 
+        print(item.c1, item.c2, item.c3, item.c4)
 
 
